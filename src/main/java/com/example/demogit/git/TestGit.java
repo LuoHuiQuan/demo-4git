7a32414ff5384df4408eda07e75916a0cdf0760f
lhq-git <lhq-git@qq.com>
2022-09-23 23:25:20 +0800

    idea first commit

---
+++ src/main/java/com/example/demogit/git/TestGit.java
@@ -0,0 +1,16 @@
+package com.example.demogit.git;
+
+/**
+ * @author lhq xxx@qq.com
+ * @version 2022/9/23 23:21
+ * @since JDK16
+ */
+public class TestGit {
+
+
+    public static void main(String[] args){
+        System.out.println("idea git commit");
+    }
+
+}
+
